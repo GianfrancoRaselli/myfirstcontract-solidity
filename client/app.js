4823cff26f80789d7f0431218d7ceb22a92eca5d
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-12-21 23:57:29 -0300

    updates

--- client/app.js
+++ client/app.js
@@ -1,23 +1,20 @@
 App = {
 
   web3Provider: {},
-  web3: {},
   account: {},
   contracts: {},
   tasksContract: {},
 
   init: async () => {
-    await App.loadEtherum();
-    await App.loadAccount();
+    App.loadEtherum();
     await App.loadTasksContract();
-    App.render();
+    App.renderTasks();
   },
 
   loadEtherum: async () => {
     if (window.ethereum) {
       App.web3Provider = window.ethereum;
-    } else if (window.web3) {
-      App.web3 = new web3(window.web3.currentProvider);
+      App.loadAccount();
     } else {
       alert('No ethereum browser is installed. Try it installing MetaMask');
     }
@@ -26,6 +23,7 @@ App = {
   loadAccount: async () => {
     const accounts = await App.web3Provider.request({ method: 'eth_requestAccounts' });
     App.account = accounts[0];
+    document.getElementById('account').innerHTML = App.account;
   },
 
   loadTasksContract: async () => {
@@ -36,13 +34,55 @@ App = {
     App.tasksContract = await App.contracts.tasksContract.deployed();
   },
 
-  render: () => {
-    document.getElementById('account').innerHTML = App.account;
+  renderTasks: async () => {
+    const tasksCounter = await App.tasksContract.tasksCounter();
+    const tasksCounterNumber = tasksCounter.toNumber();
+
+    let html = '';
+
+    for(let i = 1; i <= tasksCounterNumber; i++) {
+      const task = await App.tasksContract.tasks(i);
+
+      let taskElement = `
+        <div class="card bg-dark rounded-0 mb-2">
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <span>${task.title}</span>
+
+            <div class="form-check form-switch">
+              <input class="form-check-input" data-id="${task.id}" type="checkbox" ${task.done && "checked"} onChange="App.toggleDone(this)" />
+            </div>
+          </div>
+
+          <div class="card-body">
+            <span>${task.description}</span>
+            <p class="text-muted">Task was created ${new Date(task.createdAt * 1000).toLocaleString()}</p>
+          </div>
+          
+        </div>
+      `;
+
+      html += taskElement;
+    }
+
+    document.querySelector('#tasksList').innerHTML = html;
   },
 
   createTask: async (title, description) => {
-    await App.tasksContract.createTask(title, description, { from: App.account });
+    if (App.account) {
+      await App.tasksContract.createTask(title, description, { from: App.account });
+
+      App.renderTasks();
+    }
   },
+
+  toggleDone: async (element) => {
+    if (App.account) {
+      const taskId = element.dataset.id;
+      await App.tasksContract.toggleDone(taskId, { from: App.account });
+
+      App.renderTasks();
+    }
+  }
 
 }
 
